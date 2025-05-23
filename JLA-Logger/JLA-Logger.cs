3ef16729b10d8d5b3c368feef2abb9af2dfbf2a1
EliOlsen <eli.f.olsen.mh@gmail.com>
2025-05-23 14:20:52 -0500

    Refacted JLA-Logger to use JLA-Library

--- JLA-Logger/JLA-Logger.cs
+++ JLA-Logger/JLA-Logger.cs
@@ -1,19 +1,15 @@
 ﻿using System.Text;
-using System.Text.Json;
-using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using JLALibrary;
-
 namespace JLALogger;
-
 public class JLALogger
 {
     public static async Task Main()
     {
         //Before anything else: Get configuration data from config file.
         string configFilePath = Path.Combine(Environment.CurrentDirectory, "config.json");
-        JLALoggerConfiguration defaultConfiguration = new()
+        JLALoggerConfiguration DefaultConfiguration = new()
         {
             UserName = "guest", //RabbitMQ default username upon installation
             Password = "guest", //RabbitMQ default password upon installation
@@ -22,46 +18,7 @@ public class JLALogger
             LogFolderPath = Path.Combine(Environment.CurrentDirectory, "logs"),
             LogQueueBindingKeys = ["#"]
         };
-        if (!File.Exists(configFilePath))
-        {
-            //If file does not exist, make a default one and close program with error message
-            OutputFormattedConsoleError("Error: configuration file not found. Generating default file at " + configFilePath + " and exiting program. Please edit config file to reflect desired configuration.");
-            using (var fs = File.Create(configFilePath))
-            {
-                //serialize default configuration object into file for user to edit (set up inside a dictionary like so for proper format used when retrieving later)
-                await JsonSerializer.SerializeAsync(fs, new Dictionary<string, JLALoggerConfiguration> { { defaultConfiguration.GetType().Name, defaultConfiguration } });
-            }
-            System.Environment.Exit(1);
-        }
-        //Knowing now that the configuration file does exist, we'll move on to building the configuration
-        var configuration = new ConfigurationBuilder().AddJsonFile(configFilePath, optional: false, reloadOnChange: true).Build();
-        JLALoggerConfiguration? settings = defaultConfiguration;
-        try
-        {
-            settings = configuration.GetRequiredSection(defaultConfiguration.GetType().Name).Get<JLALoggerConfiguration>();
-        }
-        catch (InvalidOperationException e) //The file may exist, but it's always possible it's not formatted properly
-        {
-            OutputFormattedConsoleError("configuration file improperly formatted. Specifically: " + e.ToString());
-            System.Environment.Exit(1);
-        }
-        //Just in case, let's ensure settings is always non-null and toss a warning if it wasn't somehow
-        if (settings is null)
-        {
-            settings = defaultConfiguration;
-            OutputFormattedConsoleError("Configuration file invalid; default configuration file in effect.");
-        }
-        //In the case where the user specifically did not provide one of the required values, this individual value defaults to a null.
-        //Problematic. I want to replace nulls with the default values and throw a warning if so.
-        // I COULD fix this generically by using reflection... but that's massive overkill for this project. 
-        //On the other hand, fixing it by making a bunch of in-place fallbacks is too little effort.
-        // I've added a function to the config class to correct and throw a non-stopping error to notify the user.
-        if (settings.ReplaceEmptyStrings(defaultConfiguration))
-        {
-            OutputFormattedConsoleError("Configuration file missing one or more values; default value substituted for missing value.");
-        }
-        //At last, we have a verified good (or at least non-problematic) settings object with verfied non-problematic values. We're able to continue on.
-
+        JLALoggerConfiguration settings = await new UserJsonConfiguration<JLALoggerConfiguration>().RetrieveAndValidateSettings(DefaultConfiguration, configFilePath);
         //Main functionality, getting log messages from RabbitMQ and putting them in a local file for user perusal.
         //First, set up RabbitMQ connection
         var factory = new ConnectionFactory
@@ -72,7 +29,6 @@ public class JLALogger
         };
         using var connection = await factory.CreateConnectionAsync();
         using var channel = await connection.CreateChannelAsync();
-
         await channel.ExchangeDeclareAsync(exchange: settings.LogExchangeName, type: ExchangeType.Topic);
 
         // declare a server-named queue
@@ -103,18 +59,11 @@ public class JLALogger
             Console.WriteLine($"{routingKey} - {message}");
             return Task.CompletedTask;
         };
-
         //The call to that functionality above
         await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
         //And the exit method for this command-line program
         Console.WriteLine(" [*] Waiting for messages.");
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
-    }
-    private static void OutputFormattedConsoleError(string message)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(" [.] " + message);
-        Console.ForegroundColor = ConsoleColor.White;
     }
 }
--- JLA-Logger/Objects/JLALoggerConfiguration.cs
+++ JLA-Logger/Objects/JLALoggerConfiguration.cs
@@ -8,46 +8,4 @@ public class JLALoggerConfiguration
     public required string LogExchangeName { get; set; }
     public required string LogFolderPath { get; set; }
     public required string[] LogQueueBindingKeys { get; set; }
-
-    public bool ReplaceEmptyStrings(JLALoggerConfiguration replacements)
-    {
-        bool output = false;
-        if (this.UserName is null)
-        {
-            this.UserName = replacements.UserName;
-            output = true;
-        }
-        if (this.Password is null)
-        {
-            this.Password = replacements.Password;
-            output = true;
-        }
-        if (this.HostName is null)
-        {
-            this.HostName = replacements.HostName;
-            output = true;
-        }
-        if (this.LogExchangeName is null)
-        {
-            this.LogExchangeName = replacements.LogExchangeName;
-            output = true;
-        }
-        if (this.LogFolderPath is null)
-        {
-            this.LogFolderPath = replacements.LogFolderPath;
-            output = true;
-        }
-        if (this.LogQueueBindingKeys is null)
-        {
-            this.LogQueueBindingKeys = replacements.LogQueueBindingKeys;
-            output = true;
-        }
-        else if (this.LogQueueBindingKeys.Length == 0)
-        {
-            this.LogQueueBindingKeys = replacements.LogQueueBindingKeys;
-            output = true;
-        }
-
-        return output;
-    }
 }
