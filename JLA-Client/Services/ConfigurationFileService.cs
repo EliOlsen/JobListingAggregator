a56e9d74e36aa148a82d448a693c9462afc7b7f9
EliOlsen <eli.f.olsen.mh@gmail.com>
2025-05-25 13:23:28 -0500

    Started Work on Client; Following the MVVM pattern, I've put the startup config code in a service.

---
+++ JLA-Client/Services/ConfigurationFileService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using JLALibrary;
+using System.Threading.Tasks;
+using JLAClient.Models;
+namespace JLAClient.Services;
+
+public class ConfigurationFileService()
+{
+    public static async Task<JLAClientConfiguration> RetrieveStartupConfiguration()
+    {
+        string configFilePath = Path.Combine(Environment.CurrentDirectory, "config.json");
+        JLAClientConfiguration DefaultConfiguration = new()
+        {
+            RulesSourcePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Avalonia.ClientGUI", "rules.json"),
+            ListingsSourcePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Avalonia.ClientGUI", "listings.json"),
+            AutosaveFrequencyInMilliseconds = 900000,
+            QueueName = "scratchjobs_queue",
+            Username = "guest",
+            Password = "guest",
+            HostName = "localhost",
+            LogExchangeName = "scratchjobs_log"
+        };
+        JLAClientConfiguration settings = await new UserJsonConfiguration<JLAClientConfiguration>().RetrieveAndValidateSettings(DefaultConfiguration, configFilePath);
+        return settings;
+    }
+}
