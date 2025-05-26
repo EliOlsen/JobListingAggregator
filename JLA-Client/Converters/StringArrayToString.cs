1df69c46c545a19bd7c5ca96422a2639f943645e
EliOlsen <eli.f.olsen.mh@gmail.com>
2025-05-26 14:11:16 -0500

    Create Avalonia Converter for string[] type data; because for some reason Avalonia really can't handle this nicely? This is a kludge, especially if I want users creating or editing rules in the client, but it'll work for the moment and when I'm ready for a non-kludge will be easy to modify.

---
+++ JLA-Client/Converters/StringArrayToString.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace JLAClient.Converters;
+
+public class StringArrayToString : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string[] strings && targetType.IsAssignableTo(typeof(string)))
+        {
+            //Convert string[] to a single, formatted string.
+            return string.Join(" || ", strings); //using || as my seperator so commas can easily be used in individual strings
+        }
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string combinedString && targetType.IsAssignableTo(typeof(string[])))
+        {
+            //Convert a single, formatted string to string[]
+            return combinedString.Split(" || ");
+        }
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+}
