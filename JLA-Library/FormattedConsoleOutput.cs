40f169d08eec15171bfb2524648158ac3febd716
EliOlsen <eli.f.olsen.mh@gmail.com>
2025-05-27 15:21:13 -0500

    Added a Debug color to FormattedConsoleOutput; won't be seeing much of this in future commits, but only because I'll try to clean up spontaneous debug messages. I'm using this a lot.

--- JLA-Library/FormattedConsoleOutput.cs
+++ JLA-Library/FormattedConsoleOutput.cs
@@ -1,6 +1,6 @@
 namespace JLALibrary;
 
-public static class FormattedConsoleOuptut
+public static class FormattedConsoleOutput
 {
     public static void Error(string message)
     {
@@ -11,6 +11,12 @@ public static class FormattedConsoleOuptut
     public static void Warning(string message)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(" [.] " + message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public static void Debug(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(" [.] " + message);
         Console.ForegroundColor = ConsoleColor.White;
     }
